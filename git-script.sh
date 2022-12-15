cddc1948f93edcd681092e71498d5346c225264d
Emma B. King <49696513+EmmaKingDev@users.noreply.github.com>
2022-12-15 12:09:05 +0200

    changed demo s3 bucket name

--- git-script.sh
+++ git-script.sh
@@ -6,6 +6,6 @@ do
     dt=$(date '+%d-%m-%Y-%H:%M:%S');
     echo "$dt" 
     zip -yr $dt-$repo-backup.zip ./
-    aws s3 cp $dt-$repo-backup.zip s3://test-codecommit-backups #pre-existing S3 bucket
+    aws s3 cp $dt-$repo-backup.zip s3://codecommit-backups #pre-existing S3 bucket
     i=$((i + 1));
 done
